1f252802a52ff5a7a72d61b5c569e71a834f4c99
retrofox <rdsuarez@gmail.com>
2020-07-06 21:10:06 -0300

    inline-help: format code

--- client/blocks/inline-help/inline-help-search-results.jsx
+++ client/blocks/inline-help/inline-help-search-results.jsx
@@ -55,7 +55,7 @@ function HelpSearchResults( {
 				title = translate( 'Show me where to:' );
 				break;
 			default:
-				return null
+				return null;
 		}
 
 		return (
@@ -77,7 +77,7 @@ function HelpSearchResults( {
 			} );
 
 			// push state only if it's internal link.
-			if ( ! ( /^http/.test( link ) ) ) {
+			if ( ! /^http/.test( link ) ) {
 				event.preventDefault();
 				return page( link );
 			} else {
@@ -92,7 +92,10 @@ function HelpSearchResults( {
 
 	const selectCurrentResultIndex = ( index ) => () => selectSearchResult( index );
 
-	const renderHelpLink = ( { link,  key,  description,  title,  support_type = SUPPORT_TYPE_API_HELP }, index ) => {
+	const renderHelpLink = (
+		{ link, key, description, title, support_type = SUPPORT_TYPE_API_HELP },
+		index
+	) => {
 		const addResultsSection = supportType?.current !== support_type || ! index;
 		if ( addResultsSection ) {
 			supportType.current = support_type;
@@ -112,9 +115,13 @@ function HelpSearchResults( {
 						onClick={ onLinkClickHandler }
 						title={ decodeEntities( description ) }
 					>
-						{ support_type === SUPPORT_TYPE_ADMIN_SECTION && <Gridicon icon="domains" size={ 18 } /> }
+						{ support_type === SUPPORT_TYPE_ADMIN_SECTION && (
+							<Gridicon icon="domains" size={ 18 } />
+						) }
 						<span>{ preventWidows( decodeEntities( title ) ) }</span>
-						{ support_type === SUPPORT_TYPE_ADMIN_SECTION && <Gridicon icon="chevron-right" size={ 18 } /> }
+						{ support_type === SUPPORT_TYPE_ADMIN_SECTION && (
+							<Gridicon icon="chevron-right" size={ 18 } />
+						) }
 					</a>
 				</li>
 			</Fragment>
@@ -133,15 +140,14 @@ function HelpSearchResults( {
 		return (
 			<>
 				{ ! isEmpty( searchQuery ) && ! hasAPIResults && (
-					<p className="inline-help__empty-results">{
-						translate( 'Sorry, there were no matches. Here are some of the most searched for help pages for this section:'
+					<p className="inline-help__empty-results">
+						{ translate(
+							'Sorry, there were no matches. Here are some of the most searched for help pages for this section:'
 						) }
 					</p>
 				) }
 
-				<ul className="inline-help__results-list">
-					{ searchResults.map( renderHelpLink ) }
-				</ul>
+				<ul className="inline-help__results-list">{ searchResults.map( renderHelpLink ) }</ul>
 			</>
 		);
 	};
